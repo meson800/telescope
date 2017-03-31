3a212fb41e7d5b4bd28a93f5965c1a797d3d7b23
Christopher Johnstone <meson800@gmail.com>
2017-03-30 22:50:55 -0700

    Added to timeline control, so we actually get text labels of timestamps

--- stargazer/TimelineControl.cpp
+++ stargazer/TimelineControl.cpp
@@ -1,5 +1,7 @@
 #include "TimelineControl.h"
 
+#include <chrono>
+
 wxBEGIN_EVENT_TABLE(TimelineControl, wxWindow)
 	EVT_PAINT(TimelineControl::OnPaint)
 	EVT_SIZE(TimelineControl::OnSize)
@@ -35,14 +37,54 @@ void TimelineControl::render(wxDC& dc)
 {
 	wxFont font(9, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL, false);
 	dc.SetFont(font);
+	dc.Clear();
 	int ourWidth, ourHeight;
 	GetSize(&ourWidth, &ourHeight);
+
+	//round to nearest minute
+	std::chrono::milliseconds lowerRounded = std::chrono::duration_cast<std::chrono::minutes>(std::chrono::milliseconds(lowerTimestamp));
+	std::chrono::time_point<std::chrono::system_clock> timestamp =
+		std::chrono::system_clock::time_point(std::chrono::duration_cast<std::chrono::seconds>(std::chrono::milliseconds(lowerTimestamp)));
+	auto time_t_timestamp = std::chrono::system_clock::to_time_t(timestamp);
+
+
+	//std::cout << "Recieved Noise Message at timestamp "
+	//	<< timestamp_str << " and frequency " << freq << " Hz, chunk ID = " << chunk_id << "\n";
 	
 	double millisToPixels = static_cast<double>(ourWidth) / static_cast<double>((upperTimestamp - lowerTimestamp));
-	for (uint64_t i = lowerTimestamp; i < upperTimestamp; i += 1000)
+	uint64_t currentLabel = lowerRounded.count();
+	std::chrono::milliseconds currentTime = lowerRounded;
+	while (currentLabel < upperTimestamp)
 	{
-		int x = static_cast<int>((i - lowerTimestamp) * millisToPixels);
-		dc.DrawLine(x, 25, x, 15);
+		//Draw the label with the timestamp
+		std::chrono::time_point<std::chrono::system_clock> currentTimestamp =
+			std::chrono::system_clock::time_point(currentTime);
+		auto time_t_timestamp = std::chrono::system_clock::to_time_t(currentTimestamp);
+		char timestamp_str [100];
+		strftime(timestamp_str, sizeof(timestamp_str), "%k:%M:%S", std::localtime(&time_t_timestamp));
+		
+		wxSize textSize = dc.GetTextExtent(timestamp_str);
+		if (currentLabel > lowerTimestamp)
+		{
+			int textX = static_cast<int>((currentLabel - lowerTimestamp) * millisToPixels) - textSize.GetWidth() / 2;
+			if (textX > 0)
+			{
+				dc.DrawText(timestamp_str, textX, 0);
+			}
+		}
+
+		//Draw line ticks for every second
+		for (int i = 0; i < 15; ++i)
+		{
+			int x = static_cast<int>((currentLabel + (1000 * i) - lowerTimestamp) * millisToPixels);
+			int height = (i == 0) ? 10 : 5;
+			if (x > 0)
+			{
+				dc.DrawLine(x, 25, x, 25 - height);
+			}
+		}
+		currentTime += std::chrono::seconds(15);
+		currentLabel = currentTime.count();
 	}
 }
 
